d38238b4621e9f4c6a7a8624091fb92b5420ade3
dudgne5845 <dudgns5845@naver.com>
2021-01-23 01:06:12 +0900

    2021-01-23
    Code up(1051~1060)

---
+++ CodeUp/기초2.입출력문 및 연산자/1054/sol_python.py
@@ -0,0 +1,5 @@
+n1,n2 = map(int, input().split(" "))
+if(n1<=n2):
+    print("1")
+else:
+    print("0")
---
+++ CodeUp/기초2.입출력문 및 연산자/1056/sol_cpp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	scanf("%d %d",&a,&b);
+	if(a<=b) printf("1");
+	else printf("0");
+	return 0;
+}
+
