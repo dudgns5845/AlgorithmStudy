ef2cf09de75a9b5a3d9b65a8be232dd2080c2353
honeyoung <dudgns5845@naver.com>
2021-01-29 16:30:37 +0900

    2021-01-29
    Code up (탐욕법)

---
+++ CodeUp/그리디/리모컨/sol_cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int count = 0;
+    int a, b;
+    scanf("%d %d",&a,&b);
+    while(a != b){
+        if(a<b){
+            
+        }
+    }
+
+    return 0;
+}
