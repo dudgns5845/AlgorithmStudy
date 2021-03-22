7c7197521692c46a142e0c372e9810b8b79dfcb3
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-03-22 15:09:14 +0900

    2021-03-22
    dp

--- 백준/DP/11726_2N타일채우기/sol_python.py
+++ 백준/DP/11726_2N타일채우기/sol_python.py
@@ -13,6 +13,6 @@ dp = [0]*1001
 dp[1] = 1
 dp[2] = 2
 for i in range(3,n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+    dp[i] = (dp[i-1] + dp[i-2])%10007
 
-print(dp[n])
+print(dp[n]%10007)
--- 백준/DP/9095_123더하기/sol_python.py
+++ 백준/DP/9095_123더하기/sol_python.py
@@ -15,7 +15,7 @@ dp[2] = 2
 dp[3] = 4
 
 for i in range(4,12):
-    dp[i] = dp[i-1] + dp[i-2] + dp[i+3]
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 
 for _ in range(T):
     n = int(input())
