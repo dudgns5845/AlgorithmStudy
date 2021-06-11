6add37ef3464817df363d8376ddde8c6fcdba689
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-06-12 04:24:34 +0900

    2021-6-12
    백준 9184 신나는 함수 실행

---
+++ 백준/DP/9184_신나는 함수 실행/sol_python.py
@@ -0,0 +1,35 @@
+import sys
+
+# 3차원 메모리 제이션 (a,b,c) 0 <= a,b,c <= 20
+dp = [[[0] * 21 for _ in range(21)] for _ in range(21)]
+
+def w(a,b,c):
+
+    if a <= 0 or b<=0 or c<=0:
+        return 1
+    if a > 20 or b > 20 or c > 20:
+        return w(20,20,20)
+
+    #메모리제이션 값이 있다면 값을 리턴
+    if dp[a][b][c]:
+        return dp[a][b][c]
+
+    #값이 없을 경우 계산
+    #경우 1
+    if a < b < c:
+        dp[a][b][c] = w(a,b,c-1) + w(a,b-1,c-1) - w(a,b-1,c)
+        return dp[a][b][c] 
+    #경우 2
+    else:
+        dp[a][b][c] = w(a-1,b,c) + w(a-1,b-1,c) + w(a-1,b,c-1) - w(a-1,b-1,c-1)
+        return dp[a][b][c]
+
+while True:
+    a, b,c = map(int,input().split())
+
+    if a == -1 and b == -1 and c == -1:
+        break
+    print(f'w({a}, {b}, {c}) = {w(a,b,c)}')
+
+
+
