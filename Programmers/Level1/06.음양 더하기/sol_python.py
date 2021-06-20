11ec56c380848e3a8a6dfdb5214b12f610faeb94
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-06-20 13:41:53 +0900

    프로그래머스 구현 음양 더하기

---
+++ Programmers/Level1/06.음양 더하기/sol_python.py
@@ -0,0 +1,14 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i]  == True:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+    return answer
+
+
+    #다른 유저 풀이
+    #zip(list_1, list_2)를 하면 
+    #(1,1) (2,2) 이런식으로 반환해준다
+    #for i in zip(list,list)
