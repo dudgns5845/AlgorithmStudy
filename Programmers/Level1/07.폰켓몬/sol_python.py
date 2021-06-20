2ff3228754a5334bb7aab618e6679d83d2efac85
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-06-20 21:33:34 +0900

    프로그래머스 자료구조 폰켓몬

---
+++ Programmers/Level1/07.폰켓몬/sol_python.py
@@ -0,0 +1,14 @@
+def solution(nums):
+    answer = 0
+    
+    #가질 수 있는 포켓몬의 수
+    total_count = len(nums)//2
+    
+    answer = total_count
+    
+    nums = list(set(nums))
+    
+    if total_count > len(nums)
+        answer = len(nums)
+    
+    return answer
