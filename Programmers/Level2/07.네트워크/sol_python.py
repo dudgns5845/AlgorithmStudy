551736189ac60dd07e82b653b74f6873a145b383
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-06-24 16:18:51 +0900

    프로그래머스 DFS/BFS 네트워크

---
+++ Programmers/Level2/07.네트워크/sol_python.py
@@ -0,0 +1,39 @@
+#문제 https://programmers.co.kr/learn/courses/30/lessons/43162
+
+def solution(n, computers):
+    answer = 0
+
+    visited = [False] * n
+
+    # i,i 자신 간선 없앰
+    for i in range(n):
+        #방문 안했으면 진행
+        if visited[i] == False:
+            DFS(n,computers, i , visited)
+            #연결된거 다 끝났으면 count증가
+            answer += 1
+
+    return answer
+        
+        
+
+    
+                
+
+def DFS(n,computers, start, visited):
+    #방문 처리해주고
+    visited[start] = True
+
+    #순회 시작
+    for i in range(n):
+        #연결 되어있으며
+        if computers[start][i] == 1:
+            #방문을 하지 않았을때
+            if visited[i] == False:
+                #재귀 시작
+                DFS(n,computers,i,visited)
+
+n = 3
+computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+
+print(solution(n,computers))
