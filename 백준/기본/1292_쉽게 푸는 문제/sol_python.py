c5fceb0f9dd1396e75635fed3918122b2247f5a1
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-07-06 00:05:48 +0900

    백준 구현 쉽게 푸는 문제

--- 백준/기본/1292_쉽게 푸는 문제/sol_python.py
+++ 백준/기본/1292_쉽게 푸는 문제/sol_python.py
@@ -1,1 +1,8 @@
 #문제 https://www.acmicpc.net/problem/1292
+
+number_list = []
+for i in range(1, 46):
+    number_list += [i] * i
+    
+A, B = map(int, input().split())
+print(sum(number_list[A-1:B]))
