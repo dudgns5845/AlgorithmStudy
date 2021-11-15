8836b59f64d8156eadab8dc5cefc6979e4790e72
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-11-15 22:13:51 +0900

    프로그래머스 숫자 문자열과 영단어

---
+++ Programmers/Level1/13.숫자문자열과 영단어/sol_cpp.cpp
@@ -0,0 +1,21 @@
+#include <regex>
+#include <string>
+
+using namespace std;
+
+int solution(string s) {
+    s = regex_replace(s, regex("zero"), "0");
+    s = regex_replace(s, regex("one"), "1");
+    s = regex_replace(s, regex("two"), "2");
+    s = regex_replace(s, regex("three"), "3");
+    s = regex_replace(s, regex("four"), "4");
+    s = regex_replace(s, regex("five"), "5");
+    s = regex_replace(s, regex("six"), "6");
+    s = regex_replace(s, regex("seven"), "7");
+    s = regex_replace(s, regex("eight"), "8");
+    s = regex_replace(s, regex("nine"), "9");    
+    return stoi(s);
+}
+
+// 정규식
+// <regex> 클래스 공부하기
