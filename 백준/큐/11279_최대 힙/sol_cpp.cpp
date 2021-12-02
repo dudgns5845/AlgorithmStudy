b49baab1d2e6f34e7ab55e9cb357f621fd4cf185
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-12-02 11:41:59 +0900

    백준 11279 최대 힙

---
+++ 백준/큐/11279_최대 힙/sol_cpp.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main() {
+
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+	priority_queue<int> pq;
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+		if (temp == 0) {
+			if (pq.empty()) {
+				cout << 0 << "\n";
+			}
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(temp);
+		}
+	}
+}
