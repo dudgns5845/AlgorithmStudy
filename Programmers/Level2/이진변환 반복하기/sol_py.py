c2c68b7d11425afca83997d10058e32de3ca4e0f
honeyoung <dudgns5845@naver.com>
2022-11-01 18:13:18 +0900

    프로그래머스 이진변환 반복하기

---
+++ Programmers/Level2/이진변환 반복하기/sol_py.py
@@ -0,0 +1,16 @@
+def solution(s):
+    # answer[0] 횟수 answer[1] 개수
+    answer = [0,0]
+    while True:
+        print(s)
+        answer[0] += 1
+        answer[1] += s.count('0')
+        s = bin(len(s)-s.count('0'))[2:]
+        if s == "1":
+            break
+    
+    print(answer)
+    return answer
+
+
+solution("110010101001")
